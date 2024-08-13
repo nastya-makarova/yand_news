91df71396e5f11d7e42204e57261f69f86a4643f
Nastya Makarova <nastasia.konovalova@yandex.ru>
2024-08-14 01:36:14 +0400

    Added docstrings for tests content.

--- news/pytest_tests/test_content.py
+++ news/pytest_tests/test_content.py
@@ -8,6 +8,7 @@ from news.forms import CommentForm
 
 @pytest.mark.django_db
 def test_news_count(client, all_news):
+    """Метод проверяет количество новостей на главной странице."""
     url = reverse('news:home')
     response = client.get(url)
     object_list = response.context['object_list']
@@ -17,6 +18,9 @@ def test_news_count(client, all_news):
 
 @pytest.mark.django_db
 def test_news_order(client, all_news):
+    """Метод проверяет, что новости отсортированы
+    от самой свежей к самой старой.
+    """
     url = reverse('news:home')
     response = client.get(url)
     object_list = response.context['object_list']
@@ -27,6 +31,9 @@ def test_news_order(client, all_news):
 
 @pytest.mark.django_db
 def test_comments_order(all_comments, client, get_news_detail_page):
+    """Метод проверяет, что комментарии на странице отдельной новости
+    отсортированы в хронологическом порядке.
+    """
     response = client.get(get_news_detail_page)
     assert 'news' in response.context
     news = response.context['news']
@@ -38,11 +45,17 @@ def test_comments_order(all_comments, client, get_news_detail_page):
 
 @pytest.mark.django_db
 def test_anonymous_client_has_no_form(client, get_news_detail_page):
+    """Метод проверяет, что анонимному пользователю недоступна форма
+    для отправки комментария на странице отдельной новости.
+    """
     response = client.get(get_news_detail_page)
     assert 'form' not in response.context
 
 
 def test_authorized_client_has_form(author_client, get_news_detail_page):
+    """Метод проверяет, что авторизованному пользователю доступна форма
+    для отправки комментария на странице отдельной новости.
+    """
     response = author_client.get(get_news_detail_page)
     assert 'form' in response.context
     assert isinstance(response.context['form'], CommentForm)
